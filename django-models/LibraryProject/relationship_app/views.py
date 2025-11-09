506ab0df38cc7a7573be8076975257aedbad9f8a
BlessingIhejirika <onyinyechukwumihejirika@gmail.com>
2025-11-09 19:58:41 +0100

    committing the form files

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -28,19 +28,4 @@ def register_view(request):
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
 
-# User login view
-def login_view(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('list_books')  # Redirect after login
-    else:
-        form = AuthenticationForm()
-    return render(request, 'relationship_app/login.html', {'form': form})
 
-# User logout view
-def logout_view(request):
-    logout(request)
-    return render(request, 'relationship_app/logout.html')
