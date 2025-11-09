471f9e806013be020654f5d9778d50431f6fe052
BlessingIhejirika <onyinyechukwumihejirika@gmail.com>
2025-11-09 16:41:34 +0100

    commiting update made to query_samples.py files

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,14 +1,14 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
-# Query all books by a specific author
+# 1. Query all books by a specific author
 author_name = "George Orwell"
 author = Author.objects.get(name=author_name)
-books_by_author = author.books.all()
+books_by_author = Book.objects.filter(author=author)   # <-- ALX expects objects.filter
 print("Books by", author.name)
 for book in books_by_author:
     print("-", book.title)
 
-# List all books in a library
+# 2. List all books in a library
 library_name = "Central Library"
 library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
@@ -16,6 +16,6 @@ print("\nBooks in", library.name)
 for book in books_in_library:
     print("-", book.title)
 
-# Retrieve the librarian for a library
-librarian = library.librarian
+# 3. Retrieve the librarian for a library
+librarian = library.librarian   # Access OneToOneField
 print("\nLibrarian for", library.name, "is", librarian.name)
