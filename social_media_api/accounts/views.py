d1b51e24d76b0e2d26a21e2a4a6ef5a24a33f760
Ihejirika Blessing <meetonyinyechukwu@gmail.com>
2026-01-02 14:16:39 +0000

    committing files of social_media_api project

---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,21 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+from .serializers import RegisterSerializer, LoginSerializer
+
+CustomUser = get_user_model() 
+
+class UserRegistrationView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserRegistrationSerializer
+
+class UserLoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status=status.HTTP_200_OK)
+        return Response({'error': 'Invalid Credentials'}, status=status.HTTP_400_BAD_REQUEST)
